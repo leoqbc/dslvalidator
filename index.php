498bdb50e04846681c1a82d3b5b4f8c1176ce4a4
Leonardo Tumadjian <tumadjian@gmail.com>
2015-11-29 20:17:23 -0200

    Add all project files

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints as Assert;
+use DSLPAL\DSLValidator;
+
+
+//$constraint = [
+//    new Assert\Length([
+//        'min' => 3,
+//        'max' => 20
+//    ]),
+//    new Assert\NotBlank,
+//    new Assert\Email
+//];
+//
+//$validator = Validation::createValidator();
+//
+//$errors = $validator->validate('teste@gmail.com', $constraint);
+//
+//var_dump($errors->count());
+
+$fluent = new DSLValidator;
+
+$res = $fluent->length(['min' => 5, 'max' => 20])
+              ->notBlank()
+              ->email()
+              ->validate('teste@gmail.com')
+;
+
+var_dump($res->count());
+
+$fluent->collection([
+    'nome' => $fluent->length(['min' => 5])
+                     ->type('string')
+                    ->end(),
+    'email' => $fluent->email()
+                    ->end(),
+    'sexo' => $fluent->choice(['M', 'F'])
+                    ->end()
+]);
+
+$res = $fluent->validate([
+    'nome' => 'Leonardo',
+    'email' => 'teste@teste.com',
+    'sexo' => 'N' // Wrong!
+]);
+
+var_dump($res->count());
---
+++ src/dslpal/DSLValidator.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Created by Leonardo Tumadjian
+ * GitHub User: leoqbc
+ * Email: tumadjian@gmail.com
+ */
+
+namespace DSLPAL;
+
+use Symfony\Component\Validator\Validation;
+
+class DSLValidator
+{
+    protected $assertList;
+
+    protected $validator;
+
+    const CONSTRAINT_NAMESPACE = 'Symfony\\Component\\Validator\\Constraints\\';
+
+    public function __construct()
+    {
+        $this->validator = Validation::createValidator();
+    }
+
+    public function __call($constraint, $parameters)
+    {
+        $class = self::CONSTRAINT_NAMESPACE . ucfirst($constraint);
+        $refClass = new \ReflectionClass($class);
+        $this->assertList[] = $refClass->newInstanceArgs($parameters);
+        return $this;
+    }
+
+    public function collection(array $asserts)
+    {
+        $collection = self::CONSTRAINT_NAMESPACE . 'Collection';
+        $this->assertList = new $collection($asserts);
+    }
+
+    public function validate($value)
+    {
+        $violations = $this->validator->validate($value, $this->assertList);
+        $this->end();
+        return $violations;
+    }
+
+    public function end()
+    {
+        $assertList = $this->assertList;
+        $this->assertList = null;
+        return $assertList;
+    }
+}
